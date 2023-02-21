2f52ae8f955b458a36a101d209770fff462309f7
Leon Dong <leon.d1043@gmail.com>
2023-02-21 00:27:23 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,8 +31,8 @@ export default function Home() {
         </Head>
         <main>
             <p><a>&lt;</a>d3l n3st webring!<a>&gt;</a></p>
-            <p>to join, make a pull request <a href="https://github.com/LeonDong02/d3l-n3st/pulls">here</a> by adding your name and link to the members map in the pages/index.js file</p>
-            <p>on anywhere on your website, include the links https://d3l-n3st.vercel.app/prev and https://d3l-n3st.vercel.app/next to link forwards and backwards in the webring</p>
+            <p>to join, make a pull request <a href="https://github.com/LeonDong02/d3l-n3st/pulls">here</a> by adding your name and link to the members map in the src/members.js file</p>
+            <p>on anywhere on your website, include the links "https://d3l-n3st.vercel.app/prev" and "https://d3l-n3st.vercel.app/next" to link backwards and forwards in the webring</p>
             <p>the webring is currently invite only</p>
             <p>try out the webring here: <a href="/prev">prev</a> <a href="/next">next</a></p>
             <p>members list:</p>
