6a7fd28ff6ca69335a7bc27377aefdda618273be
Tri Ho <vinhocent@gmail.com>
2023-02-21 01:27:04 -0500

    lol funny ascii!

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
             <title>d3l n3st</title>
         </Head>
         <main>
-            <p><a>&lt;</a>d3l n3st webring!<a>&gt;</a></p>
+            <p><a>&lt;</a>d3l n3st webring⸸!<a>&gt;</a></p>
             <p>to join, make a branch <a href="https://github.com/LeonDong02/d3l-n3st">here</a> and add your name and website link to the src/members.js file, then make a pull request <a href="https://github.com/LeonDong02/d3l-n3st/pulls">here</a></p>
             <p>make sure that somewhere on your website you have the links "https://d3l-n3st.vercel.app/prev" and "https://d3l-n3st.vercel.app/next" to link backwards and forwards in the webring</p>
             <b>the webring is currently invite only</b>
